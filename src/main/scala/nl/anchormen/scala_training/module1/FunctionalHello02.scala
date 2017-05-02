9aa94f3b1b78d033dfc1113aba43f79ea51f3afd
Jeroen Vlek <j.vlek@anchormen.nl>
2017-05-02 16:00:24 +0200

    Functional Hello Anchormen Example

---
+++ src/main/scala/nl/anchormen/scala_training/module1/FunctionalHello02.scala
@@ -0,0 +1,20 @@
+package nl.anchormen.scala_training.module1
+
+/**
+  * A simple functional program displaying several uses of a function value.
+  *
+  * @author Jeroen Vlek <j.vlek@anchormen.nl>
+  */
+object FunctionalHello02 extends App {
+
+  val func : (String) => Unit = (line : String) => println(line)
+  func("Hello Anchormen!")
+
+  val aCollection = Seq("Hello",  "Anchormen!")
+  for(el <- aCollection) {
+    func(el)
+  }
+
+  aCollection.map(func)
+  aCollection map func
+}
