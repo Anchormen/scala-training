64f8c8f01deed8adc45628adc11325384d4d4e8f
Jeroen Vlek <j.vlek@anchormen.nl>
2017-06-14 15:36:14 +0200

    Added exercise skeleton

---
+++ src/main/scala/nl/anchormen/scala_training/module1/exercise/Exercise01.scala
@@ -0,0 +1,9 @@
+package nl.anchormen.scala_training.module1.exercise
+
+object Exercise01 {
+  // Create a collection of family members: Rick, Morty, Summer, Beth, Jerry
+  // Print out the collection
+  // Write a function literal that turns all family members, except Rick and Morty, into Cronenbergs by appending their name with "_Cronenberg"
+  // Create a new collection by applying the above function to the family collection
+  // Print out the result
+}
