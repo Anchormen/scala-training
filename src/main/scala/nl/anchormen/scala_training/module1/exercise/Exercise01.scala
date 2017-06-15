6924e23397872b7a1c9d087bdc867d8c8ff91f47
Jeroen Vlek <j.vlek@anchormen.nl>
2017-06-15 15:06:35 +0200

    Moved assignment to class doc

--- src/main/scala/nl/anchormen/scala_training/module1/exercise/Exercise01.scala
+++ src/main/scala/nl/anchormen/scala_training/module1/exercise/Exercise01.scala
@@ -1,9 +1,12 @@
 package nl.anchormen.scala_training.module1.exercise
 
+/**
+  * 1) Create a collection of family members: Rick, Morty, Summer, Beth, Jerry
+  * 2) Print out the collection
+  * 3) Write a function literal that turns all family members, except Rick and Morty, into Cronenbergs by appending their name with "_Cronenberg"
+  * 4) Create a new collection by applying the above function to the family collection
+  * 5) Print out the result
+  */
 object Exercise01 {
-  // Create a collection of family members: Rick, Morty, Summer, Beth, Jerry
-  // Print out the collection
-  // Write a function literal that turns all family members, except Rick and Morty, into Cronenbergs by appending their name with "_Cronenberg"
-  // Create a new collection by applying the above function to the family collection
-  // Print out the result
+  ???
 }
