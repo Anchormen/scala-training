f25a60098c6bd9110525642ee5d9a638dfe3aa47
Jeroen Vlek <j.vlek@anchormen.nl>
2017-06-18 10:03:53 +0200

    Defined Exercise 02

---
+++ src/main/scala/nl/anchormen/scala_training/module1/exercise/Exercise02.scala
@@ -0,0 +1,11 @@
+package nl.anchormen.scala_training.module1.exercise
+
+/**
+  * 1) Add the 2 dependencies needed for Scala Logging as described here: https://github.com/typesafehub/scala-logging
+  * 2) Let Exercise02 inherit the LazyLogging trait.
+  * 3) Loop over all program arguments and log them to INFO.
+  * 4) Run the program from the SBT console with the arguments "anchormen scala training" (or any other multi-word string)
+  */
+object Exercise02 {
+  ???
+}
