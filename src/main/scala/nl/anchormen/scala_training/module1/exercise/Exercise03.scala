7f78cd6740cae2684fd31f888f976cc8d042880b
Jeroen Vlek <j.vlek@anchormen.nl>
2017-06-19 11:11:42 +0200

    Exercise 03

---
+++ src/main/scala/nl/anchormen/scala_training/module1/exercise/Exercise03.scala
@@ -0,0 +1,11 @@
+package nl.anchormen.scala_training.module1.exercise
+
+/**
+  * 1) Create an abstract Shape class that has an area() method
+  * 2) Define a Circle, Triangle and Square case subclasses.
+  * 3) Create a List containing 2 instances of each subclass
+  * 4) Loop over the list and print the total area.
+  */
+object Exercise03 {
+  ???
+}
