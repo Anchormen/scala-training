b420f8194acc9179843671fd89110a1f9b910b3b
Jeroen Vlek <j.vlek@anchormen.nl>
2017-06-20 09:10:25 +0200

    Some collection method examples

---
+++ src/main/scala/nl/anchormen/scala_training/module2/example/MoreCollections01.scala
@@ -0,0 +1,34 @@
+package nl.anchormen.scala_training.module2.example
+
+/**
+  * Displays more collections types.
+  *
+  * @author Jeroen Vlek <j.vlek@anchormen.nl>
+  */
+object MoreCollections01 extends App {
+  /*** Map ***/
+  val l = List(1, 2, 3)
+  l map (
+    (i : Int) => i + 2
+  ) // List(3, 4, 5)
+  l map (_ + 2) // same thing
+
+  /*** Flatten ***/
+  val thickList = List(
+    List(1, 2, 3),
+    List(4, 5, 6),
+    List(7, 8, 9)
+  )
+  thickList flatten // List(1, 2, 3, 4, 5, 6, 7, 8, 9)
+
+  /*** Flat Map ***/
+  thickList flatMap(
+    (l : List[Int]) => l.map(
+      (i : Int) => i * 10
+    )
+  ) // List(10, 20, 30, 40, 50, 60, 70, 80, 90)
+  thickList flatMap (_.map(_* 10))  // same thing
+
+  /*** Filter ***/
+  thickList.flatten.filter(_ % 2 == 0) // List(2, 4, 6, 8)
+}
