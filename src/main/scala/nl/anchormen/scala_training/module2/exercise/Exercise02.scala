e7f92aa57286bc4f4321438cd465647fb5a5a770
Jeroen Vlek <j.vlek@anchormen.nl>
2017-06-21 17:13:13 +0200

    Excercise for second part

---
+++ src/main/scala/nl/anchormen/scala_training/module2/exercise/Exercise02.scala
@@ -0,0 +1,12 @@
+package nl.anchormen.scala_training.module2.exercise
+
+/**
+  * Write a function that takes the following list of strings and that uses the Try
+  * construct to sum all the strings that can be converted to an integer: ["1", "2", "x", "three", "3"]
+  * Strings that cannot be converted are to be ignored. The expected answer is thus 6.
+  * Tip: Use toInt to convert a String to an Integer
+  */
+object Exercise02 {
+  ???
+}
+
