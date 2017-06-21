bbd6edeeaeb5468727dae3b7f37692acf4483243
Borys Biletskyy <borys.biletskyy@gmail.com>
2017-06-21 17:02:13 +0200

    added placeholder for pattern matching exercise

---
+++ src/main/scala/nl/anchormen/scala_training/module2/exercise/Exercise03.scala
@@ -0,0 +1,9 @@
+package nl.anchormen.scala_training.module2.exercise
+
+/**
+  * Exercise for pattern matching
+  * @author Borys Biletskyy <b.biletskyy@anchormen.nl>
+  */
+object Exercise03 {
+  
+}
