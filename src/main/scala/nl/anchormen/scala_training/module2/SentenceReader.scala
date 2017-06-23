93ff3c123f965eb3905a1a216e682f7f52ea862f
Jeroen Vlek <j.vlek@anchormen.nl>
2017-06-23 09:54:21 +0200

    Added missing classpath slash

--- src/main/scala/nl/anchormen/scala_training/module2/SentenceReader.scala
+++ src/main/scala/nl/anchormen/scala_training/module2/SentenceReader.scala
@@ -4,7 +4,7 @@ import scala.io.Source
 
 object SentenceReader {
   def sentences : List[String] = {
-    val inputStream = getClass.getResourceAsStream("rick_morty.txt")
+    val inputStream = getClass.getResourceAsStream("/rick_morty.txt")
     val source = Source.fromInputStream(inputStream)
     val lineIterator = source.getLines
     lineIterator.toList
